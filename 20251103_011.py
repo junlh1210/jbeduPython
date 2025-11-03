848f102329b84b5e2caf34d46a856378348565f3
erion <junlh1210@empal.com>
2025-11-03 21:22:29 +0900

    문자열 다루기

---
+++ 20251103_011.py
@@ -0,0 +1,16 @@
+a = "Life is too short, you need Python"
+print(len(a))
+
+print(a[3])
+print(a[12])
+print(a[-1])
+print(a[-5])
+
+print("")
+
+print(a[0]+a[1]+a[2]+a[3])
+print(a[0:4])       
+print(a[0:3])
+print(a[5:7])
+print(a[5:6])
+print(a[0]+a[1]+a[3])
