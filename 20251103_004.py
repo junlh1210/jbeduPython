2f505863da8618b0349a9522d3ffe77ba78167b2
erion <junlh1210@empal.com>
2025-11-03 20:18:21 +0900

    문자 다루기

---
+++ 20251103_004.py
@@ -0,0 +1,18 @@
+print(100)
+print(100+200)
+print("abc")
+print('abc')
+print('''abc''')
+print()
+
+print("""aaaa
+bbbb
+cccc
+dddd""")
+print()
+
+print('''
+      aaaa
+      bbbb
+      cccc
+      dddd''')
