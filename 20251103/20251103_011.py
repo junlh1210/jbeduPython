2be5bf6c8337fa6e74324c8819a0acac67c0a971
erion <junlh1210@empal.com>
2025-11-04 13:25:58 +0900

    Python 레인지 확인

--- 20251103/20251103_011.py
+++ 20251103/20251103_011.py
@@ -1,16 +1,17 @@
 a = "Life is too short, you need Python"
 print(len(a))
 
-print(a[3])
-print(a[12])
-print(a[-1])
-print(a[-5])
+print(a[3])     # e
+print(a[12])    # s
+print(a[-1])    # n
+print(a[-5])    # y
 
 print("")
 
+# Python 레인지 확인
 print(a[0]+a[1]+a[2]+a[3])
-print(a[0:4])       
-print(a[0:3])
-print(a[5:7])
+print(a[0:4])               # 0 ~ 3 까지
+print(a[0:3])               # 0 ~ 2 까지
+print(a[5:7])               # 5 ~ 6 까지
 print(a[5:6])
 print(a[0]+a[1]+a[3])
