f87814a9083dfd2e00e4be7fa982dbd5b42b2627
erion <junlh1210@empal.com>
2025-11-04 21:27:24 +0900

    연산자 우선순위 및 논리 연산자

--- 20251104/20251104_012.py
+++ 20251104/20251104_012.py
@@ -1,3 +1,4 @@
+# 비교연산자
 x=6
 y=2
 print(x==y) # False
@@ -6,3 +7,15 @@ print(x<y)  # False
 print(x>=y) # True
 print(x<=y) # False
 print(x!=y) # True
+
+# 연산자 우선순위 (산술연산자가 비교연산자보다 우선)
+print(2 > 1 + 1)        # False 
+print(7 // 3 == 2)      # True
+print(1 + 2 + 3 <= 6)   # True
+print(6 % 2 == 0)       # True
+
+# 논리 연산자
+x=90
+y=83
+print(x>=85 or y>=85)   # True
+print(x>=85 and y>=85)  # False
