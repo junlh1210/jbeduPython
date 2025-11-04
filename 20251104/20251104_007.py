cf6317e60854bbccb9b5552392e821c477c973d9
erion <junlh1210@empal.com>
2025-11-04 20:20:14 +0900

    input 값을 숫자로

---
+++ 20251104/20251104_007.py
@@ -0,0 +1,7 @@
+a = int(input("첫번째 값을 입력 : ")) # integer, 정수
+b = int(input("두번째 값을 입력 : ")) 
+print(f"첫번째는 {a}, 두번째는 {b} ===> 두 숫자의 합은 {a+b}")
+
+c = float(input("첫번째 값을 입력 : ")) # integer, 정수
+d = float(input("두번째 값을 입력 : ")) 
+print(f"첫번째는 {c}, 두번째는 {d} ===> 두 숫자의 합은 {c+d}")
