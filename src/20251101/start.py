34032f15eb1c7ab8bd49f9d06cc208f73fc57673
erion <junlh1210@empal.com>
2026-02-10 19:33:21 +0900

    mnist 데이터셋 추가

--- src/20251101/start.py
+++ src/20251101/start.py
@@ -26,6 +26,7 @@ import tensorflow as tf
 
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.datasets import mnist
 from prophet.plot import add_changepoints_to_plot, plot_plotly, plot_components_plotly
 from prophet import Prophet
 from openpyxl import load_workbook
