6903ae4d535810656c4e8cd33cfa6f71ffb1b7cb
erion <junlh1210@empal.com>
2026-02-24 18:45:34 +0900

    최종

--- src/20251101/start.py
+++ src/20251101/start.py
@@ -2,14 +2,13 @@
 import warnings
 warnings.filterwarnings(action='ignore') 
 
-import glob
-import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import csv
 import folium
 import datetime
-import seaborn as sns
 import scipy as sp
 import statsmodels.formula.api as smf
 import networkx as nx
@@ -24,18 +23,25 @@ import re
 import sklearn.metrics as metrics
 import yfinance as yf
 import tensorflow as tf
+import glob
 
-from keras.utils import to_categorical
+from konlpy.tag import Okt
+from konlpy.tag import Hannanum
+from collections import Counter
+from wordcloud import WordCloud
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import to_categorical
 from keras.callbacks import ModelCheckpoint,EarlyStopping
 from tensorflow.python.keras.utils import np_utils
 from keras.datasets import mnist
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Input
 from prophet.plot import add_changepoints_to_plot, plot_plotly, plot_components_plotly
 from prophet  import Prophet
 from openpyxl import load_workbook
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from keras.models import Sequential
-from keras.layers import LSTM, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
 from sklearn.decomposition import PCA
 from sklearn.datasets import load_iris, load_wine, load_breast_cancer, load_diabetes
 from folium.plugins import HeatMap 
@@ -48,8 +54,6 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 from mpl_toolkits.mplot3d import Axes3D
 from operator import itemgetter
 from PIL import Image
-from collections import Counter
-from wordcloud import WordCloud
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.model_selection import train_test_split, cross_val_score, KFold
 from sklearn.metrics import r2_score, ConfusionMatrixDisplay, confusion_matrix, accuracy_score, silhouette_score, classification_report, precision_score, recall_score, f1_score, roc_auc_score
